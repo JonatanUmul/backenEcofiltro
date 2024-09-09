f451ee9f8c9c40b136b181493000f1c8efa725c3
JonatanUmul <jumuln@miumg.edu.gt>
2024-09-09 17:41:29 -0600

    Pruebas

--- src/routes/ordenesDeTrabajo/detallados/DTHHPrueba.routes.js
+++ src/routes/ordenesDeTrabajo/detallados/DTHHPrueba.routes.js
@@ -1,11 +1,11 @@
-import { Router } from "express";
-import { getSSDTH } from "../../../../controllers/ordenesDeTrabajo/detallles/DTHHPruebas.controllers";
+// import { Router } from "express";
+// import { getSSDTH } from "../../../../controllers/ordenesDeTrabajo/detallles/DTHHPruebas.controllers";
 
 
 
-const router = Router();
+// const router = Router();
 
 
-router.get('/DTHHprueba/:fecha_creacion_inicio/:fecha_creacion_fin/:modeloUF/:turn/:horno/:id_est/:fecha_CC', getSSDTH);
+// router.get('/DTHHprueba/:fecha_creacion_inicio/:fecha_creacion_fin/:modeloUF/:turn/:horno/:id_est/:fecha_CC', getSSDTH);
 
-export default router;
+// export default router;
